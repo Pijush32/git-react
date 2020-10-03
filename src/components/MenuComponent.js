e61848ccb729c283e7c139aa24adb28581a2133a
Pijush32 <2pijushpal2016@gmail.com>
2020-10-03 18:05:52 +0530

    React Router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,40 +1,39 @@
-import React from 'react';
+import React, { Component} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
- 
-function RenderMenuItem ({dish, onClick}) {
+class Menu extends Component{
+
+	render() {
+		const dishes = this.props.dishes
+		const menu = dishes.map((dish) => { 
+
     return (
+
+            <div key = {dish.id} className = 'col-12 col-md-5 m-1'>
         <Card
-            onClick={() => onClick(dish.id)}>
+            onClick={() => this.props.onDishselect(dish)} >
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
             </CardImgOverlay>
         </Card>
-    );
-}
+     </div>
+    )
+  })
 
-   const Menu = (props) => {
-
-    const menu=props.dishes.map(dish=>{
-        return (
-            <div key={dish.id} className="col-12 col-md-5 m-1">
-               <RenderMenuItem dish={dish} onClick={props.onClick} />
-            </div>
-        );
-    });
+   
     return ( 
         <div className="container">
         <div className="row">
          {menu}
         </div>
     </div>
-     );
+     )
 
    }
 
-    
+}  
 
     
     export default Menu;
